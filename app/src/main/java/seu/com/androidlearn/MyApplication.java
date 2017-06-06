9c117281547426328a5d168b1c7b94abadc9951a
wuxaingyu <wuxiangyu@corp.netease.com>
2017-06-06 16:33:19 +0800

    避免热启动时候，由于MyApplication初始化过长而导致的白屏问题，用一张图片加载loading页面

--- app/src/main/java/seu/com/androidlearn/MyApplication.java
+++ app/src/main/java/seu/com/androidlearn/MyApplication.java
@@ -11,6 +11,10 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         try {
+            /**
+             *有些地方会把耗时的初始化操作放在IntentService中操作：
+             *http://www.jianshu.com/p/4f10c9a10ac9,个人感觉不能保证第一个Activity出现的时候已经确保都初始化完毕了。
+             */
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
