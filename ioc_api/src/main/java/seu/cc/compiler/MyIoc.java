746da7a335f1955b4ce11ea83cf0f0cf7b493512
wuxiangyu <wuyu012@126.com>
2017-07-04 20:41:20 +0800

    api是和compiler完全无关的

--- ioc_api/src/main/java/seu/cc/compiler/MyIoc.java
+++ ioc_api/src/main/java/seu/cc/compiler/MyIoc.java
@@ -2,15 +2,12 @@ package seu.cc.compiler;
 
 import android.app.Activity;
 
-import seu.cc.ProxyInfo;
-
 /**
  * Created by wuxiangyu on 2017/7/4.
  */
-
 public class MyIoc {
     public static void inject(Activity activity) {
-        String proxyClassFullName = activity.getClass().getName() + "$$" + ProxyInfo.PROXY;
+        String proxyClassFullName = activity.getClass().getName() + "$$" + "ViewInject";
         try {
             Class<?> clazz = Class.forName(proxyClassFullName);
             ViewInjector viewInjector = (ViewInjector) clazz.newInstance();
